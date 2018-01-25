f4b80bb86de5b17aaf5a9cf8702f8e97e29bc25e
tdabrowski <dabr@poczta.fm>
2018-01-25 15:12:34 +0100

    added button for generate new sentence

--- js/app.js
+++ js/app.js
@@ -2,18 +2,29 @@ $(function(){
 
     let $title = $('.title');
     let $url = 'http://api.icndb.com/jokes/random';
-    function responseHandler(response){
+    let $button = $('.generate');
+
+    function responseHandler(response,element){
         let $joke = $('<h2 class="joke">'+response.value.joke+'</h2>');
-        $title.after($joke);
+        element.after($joke);
     }
-    
-    $.ajax({
-    	url:$url,
-    	type:'GET',
-    }).done(function(response){
-        responseHandler(response);
-    }).fail(function(error){
-    	console.log('Connection error - status: ' + error.statusText);
+
+    function loadContent(path,responseFunction,element){
+        $.ajax({
+        	url:path,
+        	type:'GET',
+        }).done(function(response){
+            responseFunction(response,element);
+        }).fail(function(error){
+        	console.log('Connection error - status: ' + error.statusText);
+        });
+    }
+
+    loadContent($url,responseHandler,$title);
+
+    $button.on('click',(e)=>{
+        $title.next().remove();
+        loadContent($url,responseHandler,$title);
     });
 
 
